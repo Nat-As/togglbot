7bff1442b811e3c64782039e9ec14e4b01d76ba2
James Andrews <42288915+Nat-As@users.noreply.github.com>
2019-01-10 12:51:21 -0500

    Fixed HTTPS format, modules added

--- togglbot.py
+++ togglbot.py
@@ -11,8 +11,13 @@ import re
 import random
 
 from urlparse import urlparse
+from six.moves.urllib.parse import urlparse
 from random import randint
 from HTMLParser import HTMLParser
+if ((3, 0) <= sys.version_info <= (3, 9)):
+    from urllib.parse import urlparse
+elif ((2, 0) <= sys.version_info <= (2, 9)):
+    from urlparse import urlparse
 
 # SEED
 seed = (randint(10,60))
@@ -28,7 +33,7 @@ slot3 = abs(slotv3)
 slot4 = abs(slotv4)
 slot5 = abs(slotv5)
 
-params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue','@action': 'show'})
+#params = urlparse.urlencode({'@number': 12524, '@type': 'issue','@action': 'show'})
 headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 
 # SETUP
@@ -40,23 +45,22 @@ print(r1.status,r1.reason) #200 OK
 data1 = r1.read()#content
 conn.request("GET","/")
 r1 = conn.getresponse()
-while not r1.closed:
-	print(len(r1.read(200)))#200 bytes
-	print("Bytes recieved")
+#while not r1.closed:
+print(len(r1.read(200)))#200 bytes
+print("Bytes recieved")
 
 #LOGIN
 #https://togglstats.toggl.com/in.php?site_id=100991496&type=ping&jsuid=3406503203&mime=js&x=0.5637338976052283
-
-conn.request("POST", "", params, headers)
+loginparams = 
+loginheaders = 
+conn.request("POST", "", loginparams, loginheaders)
 response = conn.getresponse()
 print("Login status:\n")
 print(response.status, response.reason) #Login Status
 #print(response.read())
-
 
 #Logout
 now = datetime.datetime.now()#note the time
 HTTPConnection.close('www.toggl.com')
 print("Logout")
 print(now.month,now.day,now.year)
-
